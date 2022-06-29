845b1d5223ef1ccceaf257b72e52a85ac2e5768b
Well Luiz <wellington.luiz@bldlogistica.com.br>
2022-06-28 21:55:25 -0300

    finalizando desenvolvimento simples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'todolist';
+  title = 'To do List Universidade MV';
+  list:any[]=[];
+
+  addTask(item:string)
+  {
+    console.warn(item);
+    this.list.push({id:this.list.length,name:item})
+    console.log(this.list);
+  }
+  removeTask(id:number)
+  {
+    this.list=this.list.filter(item=>item.id !== id);
+  }
 }
