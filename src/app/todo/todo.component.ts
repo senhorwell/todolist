5092f3d780657c8eb2772c8be8dc3f93093eef84
Vitor Maia <vitor2001bidi@gmail.com>
2022-07-05 11:07:59 -0300

    Melhorias de layout

---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,13 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.scss']
+})
+export class TodoComponent {
+
+  @Input() content: string = ''
+  @Output() removeEvent = new EventEmitter()
+
+}
